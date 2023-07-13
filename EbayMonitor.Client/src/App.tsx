4b82fe15425086b79c3bd46198c261fd73bd4730
josuepalomodev <josuepalomodev@gmail.com>
2023-07-13 17:16:26 -0500

    add user input search and filter fields
    - Added user input fields allowing users to search for a particular item and apply filters to listings returned.

--- EbayMonitor.Client/src/App.tsx
+++ EbayMonitor.Client/src/App.tsx
@@ -1,27 +1,63 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { EbayListing } from "./types.ts";
 import format from "date-fns/format";
 import { parseISO } from "date-fns";
 
 export default function App() {
   const [ebayListings, setEbayListings] = useState<EbayListing[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [keywords, setKeywords] = useState<string>("");
+  const [totalPriceRange, setTotalPriceRange] = useState<[string, string]>([
+    "",
+    "",
+  ]);
+  const [sortBy, setSortBy] = useState<string>("totalPriceAsc");
+  const [salesTaxRate, setSalesTaxRate] = useState<string>("");
 
-  useEffect(() => {
-    async function fetchEbayListings(): Promise<void> {
-      const serverApiEndpointTest =
-        "https://localhost:44313/EbayListings?searchQuery=6700xt";
-      try {
-        const response = await fetch(serverApiEndpointTest);
-        if (response.ok) {
-          setEbayListings((await response.json()) as EbayListing[]);
-        }
-      } catch (e) {
-        console.log(e);
+  async function fetchEbayListings(): Promise<void> {
+    const [positiveKeywords, negativeKeywords] = parseKeywords(keywords);
+
+    const serverApiEndpointTest = `https://localhost:44313/EbayListings?searchQuery=${searchQuery}${
+      negativeKeywords ? `&negativeKeywords=${negativeKeywords}` : ""
+    }${positiveKeywords ? `&positiveKeywords=${positiveKeywords}` : ""}${
+      isNaN(Number(totalPriceRange[0])) || totalPriceRange[0] === ""
+        ? ""
+        : `&minTotalPrice=${Number(totalPriceRange[0])}`
+    }${
+      isNaN(Number(totalPriceRange[1])) || totalPriceRange[1] === ""
+        ? ""
+        : `&maxTotalPrice=${Number(totalPriceRange[1])}`
+    }${
+      isNaN(Number(salesTaxRate)) || salesTaxRate === ""
+        ? ""
+        : `&salesTaxRate=${Number(salesTaxRate)}`
+    }&sort=${sortBy}`;
+    console.log(serverApiEndpointTest);
+    try {
+      const response = await fetch(serverApiEndpointTest);
+      if (response.ok) {
+        setEbayListings((await response.json()) as EbayListing[]);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function parseKeywords(keywordString: string): [string, string] {
+    const keywordList: string[] = keywordString.split(",");
+    const positiveKeywords: string[] = [];
+    const negativeKeywords: string[] = [];
+
+    for (const keyword of keywordList) {
+      if (keyword.startsWith("+")) {
+        positiveKeywords.push(keyword.slice(1));
+      } else if (keyword.startsWith("-")) {
+        negativeKeywords.push(keyword.slice(1));
       }
     }
 
-    fetchEbayListings();
-  }, []);
+    return [positiveKeywords.join("+"), negativeKeywords.join("+")];
+  }
 
   return (
     <div className="min-h-screen max-w-screen text-base text-neutral-300 bg-neutral-900">
@@ -29,6 +65,101 @@ export default function App() {
         <h4 className="text-xl">Feed</h4>
       </div>
       <div className="px-4 mb-2">
+        <div className="space-y-2 mb-8">
+          <div className="flex flex-col space-y-1">
+            <label className="text-sm" htmlFor="searchQueryInput">
+              Query
+            </label>
+            <input
+              className="h-8 px-2 outline outline-1 outline-neutral-600 text-sm bg-transparent rounded"
+              id="searchQueryInput"
+              type="text"
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label className="text-sm" htmlFor="keywordsInput">
+              Keywords
+            </label>
+            <input
+              className="h-8 px-2 outline outline-1 outline-neutral-600 text-sm bg-transparent rounded"
+              id="keywordsInput"
+              type="text"
+              onChange={(e) => setKeywords(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label className="text-sm" htmlFor="minItemPrice">
+              Min Total Price
+            </label>
+            <input
+              className="h-8 px-2 outline outline-1 outline-neutral-600 text-sm bg-transparent rounded"
+              id="minItemPrice"
+              type="text"
+              onChange={(e) =>
+                setTotalPriceRange([e.target.value, totalPriceRange[1]])
+              }
+            />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label className="text-sm" htmlFor="maxItemPrice">
+              Max Total Price
+            </label>
+            <input
+              className="h-8 px-2 outline outline-1 outline-neutral-600 text-sm bg-transparent rounded"
+              id="maxItemPrice"
+              type="text"
+              onChange={(e) =>
+                setTotalPriceRange([totalPriceRange[0], e.target.value])
+              }
+            />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label className="text-sm" htmlFor="salesTaxRate">
+              Sales Tax Rate
+            </label>
+            <input
+              className="h-8 px-2 outline outline-1 outline-neutral-600 text-sm bg-transparent rounded"
+              id="salesTaxRate"
+              type="text"
+              onChange={(e) => setSalesTaxRate(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label className="text-sm" htmlFor="maxItemPrice">
+              Sort by
+            </label>
+            <select
+              className="h-8 px-2 outline outline-1 outline-neutral-600 text-sm bg-transparent rounded"
+              id="sortBySelect"
+              onChange={(e) => {
+                setSortBy(e.target.value);
+              }}
+            >
+              <option className="" value="totalPriceAsc">
+                Total Price Asc
+              </option>
+              <option className="w-full" value="totalPriceDesc">
+                Total Price Desc
+              </option>
+              <option className="w-full" value="newest">
+                Newest
+              </option>
+              <option className="w-full" value="oldest">
+                Oldest
+              </option>
+            </select>
+          </div>
+          <button
+            className="w-full h-8 px-4 text-sm text-white bg-blue-600 rounded-full"
+            onClick={async () => {
+              await fetchEbayListings();
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <h4 className="text-sm mb-2">Showing {ebayListings.length} results</h4>
         <ul className="">
           {ebayListings.map((ebayListing) => {
             return (
